b5f524da1fd71bb75c1ef8120c3336e3ce6c0984
Evance975 <evanceomolo975@gmail.com>
2024-01-23 10:24:56 +0300

    second commit

--- lora.py
+++ lora.py
@@ -1,6 +1,5 @@
 import time
 import socket
-import subprocess
 import paho.mqtt.client as mqtt
 
 
@@ -11,14 +10,16 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "IP Address request"
 
 
-# getting local IP addresses
+# getting local IP address
 
 def get_ip_address():
     try:
         # Using socket to get the local IP address
 
         skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # Connect to Google's DNS server to get local IP address
+
+        # Connecting to Google DNS server to get local IP address
+
         skt.connect(("8.8.8.8", 80))
         ip_address = skt.getsockname()[0]
         skt.close()
@@ -28,22 +29,22 @@ def get_ip_address():
         return None
 
 
-# Function connecting to the MQTT broker
+# Function for connecting to the MQTT broker
 def connect_to_mqtt():
     client = mqtt.Client()
 
     while True:
         try:
-            print("Connecting to MQTT broker...")
+            print("Connecting to MQTT broker....")
             client.connect(MQTT_BROKER, MQTT_PORT, 60)
-            print("Successfully connected to MQTT broker.")
+            print("Successfully connected to MQTT broker 😊.")
             return client
         except Exception as e:
             print(f"Connection to broker failed. Retrying again in 5 seconds... ({e})")
             time.sleep(5)
 
 
-# Function to publish the IP address to the specified topic
+# Function for publishing the IP address to the specified topic
 def publish_ip_address(client, ip_address):
     try:
         print(f"Publishing IP address ({ip_address}) to topic: {MQTT_TOPIC}")
@@ -65,10 +66,10 @@ def main():
                 # Publish the IP address to the MQTT topic
                 publish_ip_address(mqtt_client, ip_address)
 
-            # Wait for some time before checking again
+            # Waiting for some time before checking again
             time.sleep(60)
         except KeyboardInterrupt:
-            print("Exiting...")
+            print("Exiting program...")
             break
         except Exception as e:
             print(f"An error occurred: {e}")
